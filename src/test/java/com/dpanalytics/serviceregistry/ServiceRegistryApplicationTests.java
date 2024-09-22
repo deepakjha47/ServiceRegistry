7ba0633a5d8109db6c0971213bd4522f4888bff8
djha93486 <djha93486@gmail.com>
2024-09-22 13:33:24 +0530

    initial setup for eureka server to manage different microservices

---
+++ src/test/java/com/dpanalytics/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpanalytics.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
